c536a12685811feef55c86466b2463205b236a6e
Ahmed <abch8080@gmail.com>
2018-04-06 06:24:12 +0200

    added input

--- print_hello.cpp
+++ print_hello.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
 int main() {
-	std::cout << "hello worlds" << std::endl;
+	int input = 0;
+	std::cout << "please enter a number: ";
+	std::cin >> input;
+	std::cout << "hello world. Your input was: "
+		<< input << std::endl;
 }
