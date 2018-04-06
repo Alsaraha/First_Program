6ce1481385caa44c8fd11f7c3f8bef58c905c880
Ahmed <abch8080@gmail.com>
2018-04-06 08:29:17 +0200

    test

--- print_hello.cpp
+++ print_hello.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int main() {
+	struct winsize w;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
 	int input = 0;
-	std::cout << "please enter a number: ";
-	std::cin >> input;
-	std::cout << "hello world. Your input was: "
-		<< input << std::endl;
+
+	for (int i = 0; i < w.ws_col; i++) {
+		std::cout << "-";
+	}
+	for (int j = 1; j < w.ws_row-2; j++) {
+		std::cout << "-";
+		for (int i = 1; i < w.ws_col-1; i++) {
+			std::cout << " ";
+		}
+		std::cout << "-";
+		std::cout << std::endl;
+	}
+	for (int i = 0; i < w.ws_col; i++) {
+		std::cout << "-";
+	}
+	// std::cout << "please w.ws_row enter a number: " << w.ws_row;
+	// std::cin >> input;
+	// std::cout << "hello world. Your input was: "
+	//	<< input << std::endl;
 }
