e70677e90156bfc6aba2ddaad9e5558e73e0424d
Ahmed <abch8080@gmail.com>
2018-04-06 08:29:30 +0200

    test

--- print_hello.cpp
+++ print_hello.cpp
@@ -2,27 +2,23 @@
 #include <sys/ioctl.h>
 #include <stdio.h>
 #include <unistd.h>
+int check(int x, int y) {
+	if (x%4 == 0 && y%4 == 0 || )
+	   )
+}
 
 int main() {
+	#ifdef __linux__
 	struct winsize w;
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
 	int input = 0;
-
+	
 	for (int i = 0; i < w.ws_col; i++) {
-		std::cout << "-";
-	}
-	for (int j = 1; j < w.ws_row-2; j++) {
-		std::cout << "-";
-		for (int i = 1; i < w.ws_col-1; i++) {
-			std::cout << " ";
+		for (int j = 0; j < w.ws_row; j++) {
 		}
-		std::cout << "-";
-		std::cout << std::endl;
 	}
-	for (int i = 0; i < w.ws_col; i++) {
-		std::cout << "-";
-	}
+	#endif
 	// std::cout << "please w.ws_row enter a number: " << w.ws_row;
 	// std::cin >> input;
 	// std::cout << "hello world. Your input was: "
